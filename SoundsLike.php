006e041e8f3af754af93352cd04337130a929ac5
The CodePunker <info@codepunker.com>
2015-01-13 18:31:00 +0200

    Removed the calling code

--- SoundsLike.php
+++ SoundsLike.php
@@ -63,9 +63,3 @@
     }
 
   }
-
-  $input = "The quick brown fox jumped over the lazy dog";
-  $searchAgainst = array("The quick brown cat jumped over the lazy dog", "Thors hammer jumped over the lazy dog", "The quick brown fax jumped over the lazy dog");
-
-  $SoundsLike = new SoundsLike($searchAgainst, $input);
-  echo $SoundsLike->findBestMatch();
